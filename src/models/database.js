532d3c0d60ddd9df3509cb025c871e689a8beaa6
joaopombos <pv20407@alunos.estgv.ipv.pt>
2024-07-17 15:05:02 +0100

    db

--- src/models/database.js
+++ src/models/database.js
@@ -1,9 +1,9 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize({
-  database: "warebd",
-  username: "warebd_user",
-  password: "MkoQDynXsw6PcSzyF1hHhi4aBPTZWUeh",
+  database: "basededados_mqvg",
+  username: "dpg-cqbstdogph6c73c70jb0-a",
+  password: "1qkb2SBgZDFSwZnR5dI0fRkNKbuvWn5s",
   host: "dpg-cpup1qqj1k6c738f3fbg-a",
   port: 5432,
   dialect: "postgres",
